c79bf798bc24d2dd95123ecdcb2da8f9dbad34e3
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-06-10 18:27:04 +0100

    Feat: completed game

---
+++ 000-Extras/start.py
@@ -0,0 +1,7 @@
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your right and left")
+    print("Which one do you take?")
+
+    choice = input("> ")
+    return choice

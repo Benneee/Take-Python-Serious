ca2616d49a459c44be9af7c9c470e176400269f1
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-06-10 06:10:36 +0100

    Feat: extras from LPTHW

---
+++ 000-Extras/fun_with_functions3.py
@@ -0,0 +1,22 @@
+from sys import argv
+
+script, encoding, error = argv
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<==>", cooked_string)
+
+languages = open('languages.txt');
+
+main(languages, 'utf_8', error);

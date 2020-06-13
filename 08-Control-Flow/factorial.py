d061689dfd440661af3bac119a194cec560758c1
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-06-13 02:13:16 +0100

    Update: refactored factorial

--- 08-Control-Flow/factorial.py
+++ 08-Control-Flow/factorial.py
@@ -2,8 +2,13 @@ def factorial(num: int):
     if num == 1:
         return num
     product = num * factorial(num - 1)
-    print(product)
+    # print(product)
     return product
 
 
+# refactored
+def refactorial(num: int):
+    return num if num == 1 else (num * refactorial(num - 1)) 
+
 print(factorial(5))
+print(refactorial(5))

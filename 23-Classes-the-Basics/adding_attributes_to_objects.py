f6a18554ed599595011ee9ee0bddb07e7e5361e4
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-06-23 08:56:22 +0100

    WIP: classes in python

---
+++ 23-Classes-the-Basics/adding_attributes_to_objects.py
@@ -0,0 +1,13 @@
+# Attributes are public by default and they can be accessed using the dot syntax
+class Guitar():
+  def __init__(self):
+    print(f'A new guitar is being created. This object is {self}')
+
+acoustic = Guitar()
+electric = Guitar()
+
+acoustic.wood = 'Mahogany'
+acoustic.strings = 6
+acoustic.year = 1990
+
+electric.nickname = 'Sound Viking 3000'

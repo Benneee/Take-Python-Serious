0b4139dae79f56d152729d08f9df324faa49521c
Benedict <benedictiknkeonye@gmail.com>
2021-10-14 01:24:13 +0100

    Feat: set up and tear down

---
+++ 30-Testing-Code-The-Basics/setup-and-teardown.py
@@ -0,0 +1,55 @@
+# Set up and tear down are used to create test fixtures
+# A test fixture is a piece of code that constructs and configures an object or system under test
+
+import unittest
+
+class Address():
+    def __init__(self, city, state):
+        self.city = city
+        self.state = state
+
+class Owner():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Restaurant():
+    def __init__(self, address, owner):
+        self.address = address
+        self.owner = owner
+
+    @property
+    def owner_age(self):
+        return self.owner.age
+
+    @property
+    def summary(self):
+        return f"This restaurant is owned by {self.owner.name} and is located in {self.address.city}"
+
+
+class TestRestaurant(unittest.TestCase):
+    # These execute before and after each test respectively
+    # setUp sets up every necessary functionality for a test
+    def setUp(self):
+        print('Runs before each test')
+        address = Address(city = 'Ogudu', state = 'Lagos')
+        owner = Owner(name = 'Jack', age = 60)
+        # for variables like the one below, it's best to have it assigned to "self" so it doesn't get lost to memory
+        self.golden_palace = Restaurant(address, owner)
+
+    def tearDown(self):
+        print('Runs after each test')
+
+
+    def test_owner_age(self):
+        self.assertEqual(self.golden_palace.owner_age, 60)
+
+    def test_summary(self):
+        self.assertEqual(
+            self.golden_palace.summary,
+            'This restaurant is owned by Jack and is located in Ogudu'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()

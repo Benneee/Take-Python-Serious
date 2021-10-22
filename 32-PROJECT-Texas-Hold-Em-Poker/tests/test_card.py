8dab596f7e8821dee7ecfa226dd5240d9cc9e00c
Benedict <benedictiknkeonye@gmail.com>
2021-10-22 13:43:40 +0100

    Feat: str and repr methods added to Card class

--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_card.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_card.py
@@ -8,3 +8,12 @@ class CardTest(unittest.TestCase):
     def test_has_suit(self):
         card = Card(rank = "2", suit = "Clubs")
         self.assertEqual(card.suit, "Clubs")
+
+    def test_has_string_representation(self):
+        card = Card("5", "Diamonds")
+        # str(card) should give us the string representation of the card object as created in the Card class with the dunder string, __str__, method
+        self.assertEqual(str(card), "5 of Diamonds") 
+
+    def test_has_technical_representation(self):
+        card = Card("5", "Diamonds")
+        self.assertEqual(repr(card), "Card('5', 'Diamonds')")

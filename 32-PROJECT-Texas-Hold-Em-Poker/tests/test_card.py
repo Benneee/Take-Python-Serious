fe4f909b16030b6cecdb0cb4647839e9b107d039
Benedict <benedictiknkeonye@gmail.com>
2021-10-22 12:33:12 +0100

    Feat: card class

--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_card.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_card.py
@@ -1,5 +1,10 @@
 import unittest
-
+from poker.card import Card
 class CardTest(unittest.TestCase):
-    def test_setup(self):
-        self.assertEqual(1, 1)
+    def test_has_rank(self):
+        card = Card(rank = "Queen", suit = "Hearts")
+        self.assertEqual(card.rank, "Queen")
+
+    def test_has_suit(self):
+        card = Card(rank = "2", suit = "Clubs")
+        self.assertEqual(card.suit, "Clubs")

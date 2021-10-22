5d8bd990a272ff06f50b265682ba669cea983765
Benedict <benedictiknkeonye@gmail.com>
2021-10-22 19:49:00 +0100

    Feat: main.py

---
+++ 32-PROJECT-Texas-Hold-Em-Poker/main.py
@@ -0,0 +1,6 @@
+from poker.card import Card
+
+card1 = Card("2", "Spades")
+card2 = Card("Ace", "Hearts")
+
+#  from main import card1, card2
--- 32-PROJECT-Texas-Hold-Em-Poker/poker/card.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/card.py
@@ -3,7 +3,7 @@ class Card():
     
     RANKS = (
                 "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                "Jack", "Queen", "King"
+                "Jack", "Queen", "King", "Ace"
             )
 
     def __init__(self, rank, suit):

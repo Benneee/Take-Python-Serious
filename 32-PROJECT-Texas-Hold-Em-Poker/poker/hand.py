9f49c83af75d21a31f032c2e24c031141cd87318
Benedict <benedictiknkeonye@gmail.com>
2021-11-02 21:57:09 +0100

    Feat: the two pair rank

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
@@ -3,17 +3,28 @@ class Hand():
         self.cards = cards
 
     def best_rank(self):
-        card_rank_counts = {}
+        ranks_with_pairs = { 
+            rank: rank_count
+            for rank, rank_count in self._card_rank_counts.items()
+            if rank_count == 2
+        }
 
+        if len(ranks_with_pairs) == 2:
+            return "Two Pair"
+        
+        if len(ranks_with_pairs) == 1:
+            return "Pair"
+
+        
+        return "High Card"
+
+    @property
+    def _card_rank_counts(self):
+        card_rank_counts = {}
         for card in self.cards:
             card_rank_counts.setdefault(card.rank, 0)
             card_rank_counts[card.rank] += 1
-
-        for rank_count in card_rank_counts.values():
-            if rank_count == 2:
-                return "Pair"
-
-        return "High Card"
+        return card_rank_counts
 
 # setdefault sets a key with its value in the dictionary if the key doesn't previously exist
 # Duplication is cheaper than the wrong abstraction

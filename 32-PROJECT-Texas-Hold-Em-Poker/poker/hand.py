6179f99c1a30a95852c18857dfebd43af6d871aa
Benedict <benedictiknkeonye@gmail.com>
2021-11-04 12:13:43 +0100

    Fix: refactoring the hand class

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/hand.py
@@ -2,22 +2,36 @@ class Hand():
     def __init__(self, cards):
         self.cards = cards
 
+    @property
+    def _rank_validations_from_best_to_worst(self):
+        return(
+            ("Three of a Kind", self._three_of_a_kind),
+            ("Two Pair", self._two_pair),
+            ("Pair", self._pair),
+            ("High Card", self._high_card)
+        )
+
     def best_rank(self):
+        for rank in self._rank_validations_from_best_to_worst:
+            # Three of a kind, self._three_of_a_kind
+            name, validator_func = rank
+            if validator_func():
+                return name
+
+    def _three_of_a_kind(self):
         ranks_with_three_of_kind = self._ranks_with_count(3)
-
-        if len(ranks_with_three_of_kind) == 1:
-            return "Three of a Kind"
-
+        return len(ranks_with_three_of_kind) == 1
+    
+    def _two_pair(self):
         ranks_with_pairs = self._ranks_with_count(2)
+        return len(ranks_with_pairs) == 2
+    
+    def _pair(self):
+        ranks_with_pairs = self._ranks_with_count(2)
+        return len(ranks_with_pairs) == 1
 
-        if len(ranks_with_pairs) == 2:
-            return "Two Pair"
-        
-        if len(ranks_with_pairs) == 1:
-            return "Pair"
-
-        
-        return "High Card"
+    def _high_card(self):
+        return True
 
     def _ranks_with_count(self, count):
         return {

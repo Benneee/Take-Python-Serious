89d23d651795003d0d7f85b7bd4cd8492280f915
Benedict <benedictiknkeonye@gmail.com>
2021-11-13 00:05:22 +0100

    Feat: added community cards to players

--- 32-PROJECT-Texas-Hold-Em-Poker/poker/game_round.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/poker/game_round.py
@@ -8,6 +8,8 @@ class GameRound():
         self._deal_initial_two_cards_to_each_player()
         self._make_bets()
 
+        self._deal_flop_cards()
+
     def _shuffle_deck(self):
         self.deck.shuffle()
     
@@ -22,3 +24,8 @@ class GameRound():
             if player.wants_to_fold():
                 self.players.remove(player)
 
+    def _deal_flop_cards(self):
+        # The players need to have the same community cards, hence not doing the assignment of the community cards variable inside the loop
+        community_cards = self.deck.remove_cards(3)
+        for player in self.players:
+            player.add_cards(community_cards)
--- 32-PROJECT-Texas-Hold-Em-Poker/tests/test_game_round.py
+++ 32-PROJECT-Texas-Hold-Em-Poker/tests/test_game_round.py
@@ -6,6 +6,24 @@ from poker.game_round import GameRound
 
 
 class GameRoundTest(unittest.TestCase):
+    def setUp(self):
+        self.first_two_cards = [
+            Card(rank = "2", suit = "Hearts"),
+            Card(rank = "6", suit = "Spades")
+        ]
+
+        self.next_two_cards = [
+            Card(rank = "9", suit = "Diamonds"),
+            Card(rank = "4", suit = "Spades")
+        ]
+
+        self.community_cards = [
+            Card(rank = "2", suit = "Diamonds"),
+            Card(rank = "4", suit = "Hearts"),
+            Card(rank = "10", suit = "Spades")
+        ]
+
+
     def test_stores_deck_and_players(self):
         deck = MagicMock()
         players = [
@@ -47,20 +65,11 @@ class GameRoundTest(unittest.TestCase):
 
     
     def test_deals_two_initial_cards_from_deck_to_each_player(self):
-        first_two_cards = [
-            Card(rank = "2", suit = "Hearts"),
-            Card(rank = "6", suit = "Spades")
-        ]
-
-        next_two_cards = [
-            Card(rank = "9", suit = "Diamonds"),
-            Card(rank = "4", suit = "Spades")
-        ]
-
         mock_deck = MagicMock()
         mock_deck.remove_cards.side_effect = [
-            first_two_cards,
-            next_two_cards
+            self.first_two_cards,
+            self.next_two_cards,
+            self.community_cards
         ]
 
         mock_player1 = MagicMock()
@@ -78,20 +87,16 @@ class GameRoundTest(unittest.TestCase):
 
         game_round.play()
 
-        # We are testing that:
-        # remove_cards was called
-        # that it was called at least twice
-        # and that the invocation had the arguments "2" in it
         mock_deck.remove_cards.assert_has_calls([
             call(2), call(2)
         ])
 
-        mock_player1.add_cards.assert_called_with(
-            first_two_cards
-        )
-        mock_player2.add_cards.assert_called_with(
-            next_two_cards
-        )
+        mock_player1.add_cards.assert_has_calls([
+            call(self.first_two_cards)
+        ])
+        mock_player2.add_cards.assert_has_calls([
+            call(self.next_two_cards)
+        ])
 
     def test_removes_player_if_not_willing_to_make_bet(self):
         deck = MagicMock()
@@ -112,3 +117,26 @@ class GameRoundTest(unittest.TestCase):
             game_round.players,
             [player2]
         )
+
+    def test_deals_same_three_commtee_cards_to_all_players_in_flop(self):
+        mock_player1 = MagicMock()
+        mock_player1.wants_to_fold.return_value = False
+        
+        mock_player2 = MagicMock()
+        mock_player2.wants_to_fold.return_value = False
+
+        players = [mock_player1, mock_player2]
+
+        mock_deck = MagicMock()
+        mock_deck.remove_cards.side_effect = [
+            self.first_two_cards,
+            self.next_two_cards,
+            self.community_cards
+        ]
+
+        game_round = GameRound(deck = mock_deck, players = players)
+        game_round.play()
+
+        mock_deck.remove_cards.assert_has_calls([call(3)])
+        mock_player1.add_cards.assert_called_with(self.community_cards)
+        mock_player2.add_cards.assert_called_with(self.community_cards)
